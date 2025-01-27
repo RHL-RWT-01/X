48dc94fa01996ef9d3aca92bb2f9600163606de7
Rahul <rahulrawat4060@gmail.com>
2025-01-27 23:24:54 +0530

    Add functionalities to fetch posts of users

--- Desktop/X/backend/routes/post.routes.js
+++ Desktop/X/backend/routes/post.routes.js
@@ -1,12 +1,25 @@
-import express from 'express';
-import { protectedRoute } from '../middlewares/protectedRoute.js';
-import { createPost } from '../controllers/post.controller.js';
+import express from "express";
+import { protectedRoute } from "../middlewares/protectedRoute.js";
+import {
+  commentOnPost,
+  createPost,
+  deletePost,
+  getAllPosts,
+  getFollowingPosts,
+  getLikedPosts,
+  getUserPosts,
+  likeUnlikePost,
+} from "../controllers/post.controller.js";
 
 const router = express.Router();
 
-router.post('/create',protectedRoute, createPost);
-// router.post('/like/:id',protectedRoute, likeUnlikePost);
-// router.post('/comment/:id',protectedRoute, commentOnPost);
-// router.delete('/delete',protectedRoute, deletePost);
+router.get("/all", protectedRoute, getAllPosts);
+router.get("/following", protectedRoute, getFollowingPosts);
+router.get("/likes/:id", protectedRoute, getLikedPosts);
+router.get("/user/:username", protectedRoute, getUserPosts);
+router.post("/create", protectedRoute, createPost);
+router.post("/like/:id", protectedRoute, likeUnlikePost);
+router.post("/comment/:id", protectedRoute, commentOnPost);
+router.delete("/:id", protectedRoute, deletePost);
 
 export default router;
