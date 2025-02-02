a3eed6f53c9b227db140e24c1b994af0ab209979
Rahul Rawat <rahulrawat4060@gmail.com>
2025-02-02 23:45:59 +0530

    created useFollow hook for reusability

---
+++ Desktop/X/frontend/src/hooks/useFollow.jsx
@@ -0,0 +1,32 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { toast } from "react-hot-toast";
+
+export default function useFollow() {
+  const { mutate: follow, isPending } = useMutation({
+    mutationFn: async () => {
+      try {
+        const res = await fetch(`api/users/follow/&{userId}`, {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+        });
+        const data = await res.json();
+        if (!res.ok) {
+          throw new Error(data.message || "Something went wrong");
+        }
+        return data;
+      } catch (error) {
+        throw new Error(error.message);
+      }
+    },
+    onSuccess: () => {
+      Promise.all([
+        queryClient.invalidateQueries({ queryKey: ["suggestedUsers"] }),
+        queryClient.invalidateQueries({ queryKey: ["authenticatedUser"] }),
+      ]);
+      toast.success("Followed successfully");
+    },
+    onError: (error) => {
+      toast.error(error.message);
+    },
+  });
+}
