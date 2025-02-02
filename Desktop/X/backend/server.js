3862f50b202e9efe6eb344ecd7c87566ca97f928
Rahul Rawat <rahulrawat4060@gmail.com>
2025-02-02 22:56:58 +0530

    fix configurations to upload the images

--- Desktop/X/backend/server.js
+++ Desktop/X/backend/server.js
@@ -26,7 +26,7 @@ app.use(cors({
   origin: "http://localhost:3000",
   credentials: true,
 }));
-app.use(express.json());
+app.use(express.json({limit: "4mb"}));
 app.use(express.urlencoded({ extended: false })); 
 app.use(cookieParser());
 
